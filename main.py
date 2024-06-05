94c85794232f9cc892cb0b6f43a1ed98b1d565f3
Joe Linogao <linogaoj@tcd.ie>
2024-06-05 20:40:38 +0100

    Added Queue Example

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from StructureClasses.Stack import Stack
+from queue import Queue
 
-def data_structure_example():
+def stack_example():
     newStack = Stack()
     print(newStack.empty())
     newStack.push("Minecraft")
@@ -15,9 +16,49 @@ def data_structure_example():
     print(newStack.search("Minecraft"))
     print(newStack.search("Helldivers 2"))
 
+def queue_built_in_example():
+    built_in_queue = Queue(maxsize = 5) # create a queue with a max size of 5
+    print("Is this queue empty?", built_in_queue.empty())
+    built_in_queue.put("Karen")
+    built_in_queue.put("Liam")
+    built_in_queue.put("Margret")
+    built_in_queue.put("John Bone")
+    built_in_queue.put("Meself")
+    print("Is this queue empty?", built_in_queue.empty(), built_in_queue, built_in_queue.full())
+    
+    if built_in_queue.full():
+        print("queue is full")
+    else:
+        built_in_queue.put("this shouldn't be possible")
+    
+
+    print(list(built_in_queue.queue), built_in_queue.qsize())
+    while built_in_queue.empty() == False:
+        print(built_in_queue.get())
+        print(list(built_in_queue.queue), built_in_queue.qsize())
+    
+    # ! note, built in Queue is non iterable. You either have to extend it or create your own one
+    class UniqueQueue:
+
+        def __init__(self):
+            self.__item_pool = []
+
+        def push(self, item):
+            if item not in self.__item_pool:
+                self.__item_pool.append(item)
+
+        def pop(self):
+            return self.__item_pool.pop()
+
+
+
 def main():
     print("Stack Data Structure (LIFO)")
-    data_structure_example()
+    stack_example()
+    
+    print("Queue Data Structure (FIFO)")
+    queue_built_in_example()
+    
     
     
 if __name__ == '__main__':
